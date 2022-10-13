0182975000f448c1a1d7085fa113377edc5c3dc2
Egor <egorstrah95@gmail.com>
2022-10-13 15:53:30 -0700

    Add Set button

--- js/script.js
+++ js/script.js
@@ -1,25 +1,38 @@
-const GRID_SIZE = 8;
+const DEFAULT_SIZE = 8;
 
 let grid = document.querySelector('.grid-container');
 
-for (let i = 0; i < GRID_SIZE; i++) {
-    for (let j = 0; j < GRID_SIZE; j++) {
-        const gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item');
-        gridItem.setAttribute('style', `height: ${100 / GRID_SIZE}%`);
-        gridItem.setAttribute('style', `width: ${100 / GRID_SIZE}%`);
-        grid.appendChild(gridItem);
-    }
+function resetGrid() {
+    grid.textContent = '';
 }
 
-let gridItems = document.querySelectorAll('.grid-item');
-
-
-
-
-gridItems.forEach(gridItem => {
-    
-    gridItem.addEventListener('mouseover', () => {
-        gridItem.style.backgroundColor = 'black';
+function setGrid(size) {
+    resetGrid();
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const gridItem = document.createElement('div');
+            gridItem.classList.add('grid-item');
+            gridItem.setAttribute('style', `height: ${100 / size}%`);
+            gridItem.setAttribute('style', `width: ${100 / size}%`);
+            grid.appendChild(gridItem);
+        }
+    }
+    let gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach(gridItem => {
+        gridItem.addEventListener('mouseover', () => {
+            gridItem.style.backgroundColor = 'black';
+        });
     });
+}
+
+setGrid(DEFAULT_SIZE);
+
+
+let setButton = document.querySelector('#set');
+setButton.addEventListener('click', () => {
+    let userInput;
+    do {
+        userInput = prompt('Grid size under 128:')
+    } while (userInput > 128);
+    setGrid(userInput);
 });
